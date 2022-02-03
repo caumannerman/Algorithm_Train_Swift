ecf275ac5df76b8be11d6bf178527e12e6521736
Caumannerman <uigohiiming2@gmail.com>
2022-02-03 15:55:09 +0900

    acmicpc#11399_ATM ( Greedy )

---
+++ Algorithm_train_Swift/Algorithm_train_Swift/acmicpc11399_ATM.swift
@@ -0,0 +1,21 @@
+//
+//  acmicpc11399_ATM.swift
+//  Algorithm_train_Swift
+//
+//  Created by 양준식 on 2022/02/03.
+//
+
+
+import Foundation
+//
+//let n: Int = Int(readLine()!)!
+//var data: Array<Int> = readLine()!.split(separator: " ").map{Int(String($0))!}
+//data.sort()
+//
+//var result: Int = 0
+//
+//for (idx, num) in data.enumerated() {
+//    result += (data.count - idx) * num
+//}
+//print(result)
+
