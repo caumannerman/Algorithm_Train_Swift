1db7aa3a49d197f9f4afd7fea0e3567c3a631d02
Caumannerman <uigohiiming2@gmail.com>
2022-02-03 15:55:24 +0900

    acmicpc#2839_설탕배달 (Greedy)

---
+++ Algorithm_train_Swift/Algorithm_train_Swift/acmicpc#2839_설탕배달.swift
@@ -0,0 +1,28 @@
+//
+//  acmicpc#2839_설탕배달.swift
+//  Algorithm_train_Swift
+//
+//  Created by 양준식 on 2022/02/03.
+//
+
+import Swift
+
+//var N = Int(readLine()!)!
+//var result = 0
+//
+//while N >= 0 {
+//    if N % 5 == 0{
+//        result += (N / 5)
+//        break
+//    }
+//    else{
+//        N -= 3
+//        result += 1
+//    }
+//}
+//if N >= 0{
+//    print(result)
+//}
+//else{
+//    print(-1)
+//}
