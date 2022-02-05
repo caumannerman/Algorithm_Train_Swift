8acd49da90f1e32cc34dd3035f8d81da153cd32e
Caumannerman <uigohiiming2@gmail.com>
2022-02-05 23:32:41 +0900

    acmicpc#2217_로프 (Greedy)

---
+++ Algorithm_train_Swift/Algorithm_train_Swift/acmicpc#2217_로프.swift
@@ -0,0 +1,24 @@
+//
+//  acmicpc#2217_로프.swift
+//  Algorithm_train_Swift
+//
+//  Created by 양준식 on 2022/02/05.
+//
+
+//import Foundation
+
+//let n: Int = Int(readLine()!)!
+//var data: Array<Int> = []
+//
+//for _ in 0..<n {
+//    data.append(Int(readLine()!)!)
+//}
+//
+//data.sort()
+//
+//var result: Int = 0
+//
+//for i in 0..<n {
+//    result = result > data[i] * ( n - i ) ? result : data[i] * (n - i)
+//}
+//print(result)
--- Algorithm_train_Swift/Algorithm_train_Swift/acmicpc#5585_거스름돈.swift
+++ Algorithm_train_Swift/Algorithm_train_Swift/acmicpc#5585_거스름돈.swift
@@ -5,17 +5,17 @@
 //  Created by 양준식 on 2022/02/05.
 //
 
-import Foundation
-
-
-var num: Int = 1000 - Int(readLine()!)!
-
-var arr: Array<Int> = [500, 100, 50, 10, 5, 1]
-
-var results: Int = 0
-
-for i in arr {
-    results += num / i
-    num %= i
-}
-print(results)
+//import Foundation
+//
+//
+//var num: Int = 1000 - Int(readLine()!)!
+//
+//var arr: Array<Int> = [500, 100, 50, 10, 5, 1]
+//
+//var results: Int = 0
+//
+//for i in arr {
+//    results += num / i
+//    num %= i
+//}
+//print(results)
