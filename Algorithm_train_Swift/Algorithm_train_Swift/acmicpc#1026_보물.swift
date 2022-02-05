a3e7736c2b664a68ee89f160250491d9c901eace
Caumannerman <uigohiiming2@gmail.com>
2022-02-05 21:59:13 +0900

    acmicpc#1026_보물(Greedy)

---
+++ Algorithm_train_Swift/Algorithm_train_Swift/acmicpc#1026_보물.swift
@@ -0,0 +1,21 @@
+//
+//  acmicpc#1026_보물.swift
+//  Algorithm_train_Swift
+//
+//  Created by 양준식 on 2022/02/05.
+//
+
+import Foundation
+//
+//let n: Int = Int(readLine()!)!
+//var a: Array<Int> = readLine()!.split(separator: " ").map{Int(String($0))!}
+//var b: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
+//
+//a.sort()
+//var result: Int = 0
+//for i in 0..<n {
+//    let now: Int = b.max()!
+//    result += a[i] * now
+//    b.remove(at: b.firstIndex(of:now)!)
+//}
+//print(result)
