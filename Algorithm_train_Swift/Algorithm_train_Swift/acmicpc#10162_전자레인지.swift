719a7892b880574d64176e312c27df64377162ab
Caumannerman <uigohiiming2@gmail.com>
2022-02-05 23:54:21 +0900

    acmicpc#10162_전자레인지(Greedy)

---
+++ Algorithm_train_Swift/Algorithm_train_Swift/acmicpc#10162_전자레인지.swift
@@ -0,0 +1,26 @@
+//
+//  acmicpc#10162_전자레인지.swift
+//  Algorithm_train_Swift
+//
+//  Created by 양준식 on 2022/02/05.
+//
+
+import Foundation
+
+var t: Int = Int(readLine()!)!
+let data: [Int] = [300, 60, 10]
+
+if t % 10 != 0 {
+    print(-1)
+    exit(0)
+}
+var results: [Int] = []
+
+for i in data {
+    results.append(t / i)
+    t %= i
+}
+
+for i in results {
+    print(i, terminator: " ")
+}
