e0a04294b945b894a1bf982bd037dccf90390248
Caumannerman <uigohiiming2@gmail.com>
2022-02-06 14:30:00 +0900

    acmicpc#13305_주유소 (Greedy)

---
+++ Algorithm_train_Swift/Algorithm_train_Swift/acmicpc#13305_주유소.swift
@@ -0,0 +1,21 @@
+//
+//  acmicpc#13305_주유소.swift
+//  Algorithm_train_Swift
+//
+//  Created by 양준식 on 2022/02/06.
+//
+
+import Foundation
+
+let n: Int = Int(readLine()!)!
+let doro: [Int] = readLine()!.split(separator: " ").map{Int(String($0))!}
+let price: Array<Int> = readLine()!.split(separator: " ").map{ Int(String($0))!}
+
+var result: Int = 0
+var min_price: Int = Int(1e9)
+
+for i in 0..<doro.count {
+    min_price = min( min_price, price[i])
+    result += min_price * doro[i]
+}
+print(result)
