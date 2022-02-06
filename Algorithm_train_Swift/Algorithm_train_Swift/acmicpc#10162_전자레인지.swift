2c4c035bd786f0c161559732b062018498136bea
Caumannerman <uigohiiming2@gmail.com>
2022-02-06 14:07:00 +0900

    acmicpc#10610_30 (Greedy)

--- Algorithm_train_Swift/Algorithm_train_Swift/acmicpc#10162_전자레인지.swift
+++ Algorithm_train_Swift/Algorithm_train_Swift/acmicpc#10162_전자레인지.swift
@@ -5,22 +5,22 @@
 //  Created by 양준식 on 2022/02/05.
 //
 
-import Foundation
-
-var t: Int = Int(readLine()!)!
-let data: [Int] = [300, 60, 10]
-
-if t % 10 != 0 {
-    print(-1)
-    exit(0)
-}
-var results: [Int] = []
-
-for i in data {
-    results.append(t / i)
-    t %= i
-}
-
-for i in results {
-    print(i, terminator: " ")
-}
+//import Foundation
+//
+//var t: Int = Int(readLine()!)!
+//let data: [Int] = [300, 60, 10]
+//
+//if t % 10 != 0 {
+//    print(-1)
+//    exit(0)
+//}
+//var results: [Int] = []
+//
+//for i in data {
+//    results.append(t / i)
+//    t %= i
+//}
+//
+//for i in results {
+//    print(i, terminator: " ")
+//}
---
+++ Algorithm_train_Swift/Algorithm_train_Swift/acmicpc#10610_30.swift
@@ -0,0 +1,22 @@
+//
+//  acmicpc#10610_30.swift
+//  Algorithm_train_Swift
+//
+//  Created by 양준식 on 2022/02/06.
+//
+
+//import Foundation
+//
+//
+//var n = Array(readLine()!).map{Int(String($0))!}
+//
+//var data = n.sorted(by: >)
+//
+//if data[data.count-1] != 0 || data.reduce(0,+) % 3 != 0 {
+//    print(-1)
+//    exit(0)
+//}
+//
+//for i in data {
+//    print(i,terminator:"")
+//}
