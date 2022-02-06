171e3acacfe842b8e8a1ff2e4e9b3367a638937e
Caumannerman <uigohiiming2@gmail.com>
2022-02-06 14:14:57 +0900

    acmicpc#1789_수들의합  (Greedy)

---
+++ Algorithm_train_Swift/Algorithm_train_Swift/acmicpc#1789_수들의합.swift
@@ -0,0 +1,21 @@
+//
+//  acmicpc#1789_수들의합.swift
+//  Algorithm_train_Swift
+//
+//  Created by 양준식 on 2022/02/06.
+//
+
+import Foundation
+
+
+//let s: Int = Int(readLine()!)!
+//
+//var now: Int = 0
+//var tick: Int = 1
+//
+//while now <= s {
+//    now += tick
+//    tick += 1
+//}
+//
+//print(tick - 2)
