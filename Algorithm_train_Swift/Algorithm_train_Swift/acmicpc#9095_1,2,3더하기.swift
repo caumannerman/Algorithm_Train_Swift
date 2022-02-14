59dae14fab4dfa9381ace7f682c4a5bba1116240
Caumannerman <uigohiiming2@gmail.com>
2022-02-14 16:44:52 +0900

    acmicpc#9095_1,2,3더하기 (Dynamic P)

---
+++ Algorithm_train_Swift/Algorithm_train_Swift/acmicpc#9095_1,2,3더하기.swift
@@ -0,0 +1,23 @@
+//
+//  acmicpc#9095_1,2,3더하기.swift
+//  Algorithm_train_Swift
+//
+//  Created by 양준식 on 2022/02/14.
+//
+
+//import Foundation
+//
+//let t: Int = Int(readLine()!)!
+//
+//var dp: [Int] = Array(repeating: 0 , count: 12)
+//dp[1] = 1
+//dp[2] = 2
+//dp[3] = 4
+//
+//for i in 4...11 {
+//    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+//}
+//for _ in 0..<t {
+//    let n: Int = Int(readLine()!)!
+//    print(dp[n])
+//}
